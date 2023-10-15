20d3e1ce89d7e52c7816d88cb234268affb30760
Bouns77 <144699498+Bouns77@users.noreply.github.com>
2023-10-15 12:04:31 +0200

    Update addUsers.sh

--- addUsers.sh
+++ addUsers.sh
@@ -1,1 +1,42 @@
+#! /bin/bash
+
+if [ -z $@ ]
+
+then
+
+        echo "Pas de nom d'utilisateurs en argument - Fin du script "
+
+        exit 1
+
+else
+
+        # vérification de l'existence de l'utilisateur
+
+        if cat /etc/passwd | grep $@ > /dev/null
+
+        then
+
+                # l'utilisateur $@ existe
+
+                echo "L'utilisateur $@ existe"
+
+  
+        else
+                for nom in "$@"
+
+                do
+
+                # création d'un utilisateur     
+
+                useradd $nom && echo "L'utilisateur $nom a été crée "
+
+                done
+
+        fi
+
+fi
+
+exit 0
+
+
 
